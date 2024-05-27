f571ad3476bb0266b1fc6de531f2862a4637e09e
MAHAMAT <138947240+mahaha2023@users.noreply.github.com>
2024-05-27 23:07:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def main():
     
     # trai/test split
     
-    y=df["Class"]
+    y=df.Class
     x=df.drop("Class",axis=1)
     x_train, x_test, y_train, y_test = train_test_split( x, y, test_size=0.2,random_state=42,stratify=y)
     st.write("X_train: ",x_train.shape)
